3ba4ae1e8088935cfdf6a10e618afead99be4cca
Shashank Patel <shashankkumar.patel@spglobal.com>
2024-07-19 17:33:45 +0530

    Add webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.tsx', '.js', '.ts', '.css', '.scss'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+};
