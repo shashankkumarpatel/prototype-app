e4145a7936ec54e16b27c100288436472243b18f
Shashank Patel <shashankkumar.patel@spglobal.com>
2024-07-19 15:40:00 +0530

    Remove unwanted files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import './App.css';
+
+/* KOI Tokens */
+import '@spglobal/tokens/dist/web/tokens.css';
+/* KOI Library utility styles */
+import '@spglobal/css-components/dist/css/koi.css';
+
 import { RouteConfig } from './configs/routeConfig';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
