7584ac55b739e5ca3fd12debf6d923a576117eca
Shashank Patel <shashankkumar.patel@spglobal.com>
2024-07-23 12:43:07 +0530

    Update Landing Page with Tile component

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,9 +1,26 @@
 import React, { FC } from 'react';
-import { Button, Card, CardBody, CardHeader, H1 } from '@spglobal/react-components';
-import { Purpose } from '@spglobal/koi-helpers';
+import {
+  Button,
+  H1,
+  Tile,
+  TileFrontGradientFade,
+  TileHover,
+  TileVariant,
+} from '@spglobal/react-components';
+import { Purpose, Size } from '@spglobal/koi-helpers';
 import { PrototypesArray } from './utils/data';
 import { useNavigate } from 'react-router-dom';
-import { CardWrapper, HeaderWrapper } from './styles/landing.styles';
+import {
+  CardWrapper,
+  CustomTileActionButtonWrapper,
+  CustomTileDescriptionHoverWrapper,
+  CustomTileDescriptionWrapper,
+  CustomTileNameHoverWrapper,
+  CustomTileNameWrapper,
+  CustomTileWrapper,
+  HeaderWrapper,
+  LandingPageWrapper,
+} from './styles/landing.styles';
 import { GeneralLayout } from '../../layouts';
 
 const LandingPage: FC = () => {
@@ -15,28 +32,51 @@ const LandingPage: FC = () => {
         <div className="spg-layout__body">
           <div className="spg-layout__main">
             <main className="spg-layout__content">
-              <HeaderWrapper>
-                <H1>Prototypes</H1>
-              </HeaderWrapper>
-              <CardWrapper>
-                {PrototypesArray.map((prototype, index) => {
-                  return (
-                    <Card key={index} hasBorder={true}>
-                      <CardHeader title={prototype.title} />
-                      <CardBody>
-                        <p>{prototype.description}</p>
-                        <Button
-                          purpose={Purpose.PRIMARY}
-                          aria-label="Load more"
-                          onClick={() => navigate(prototype.redirectionLink)}
+              <LandingPageWrapper>
+                <HeaderWrapper>
+                  <H1>Prototypes</H1>
+                </HeaderWrapper>
+                <CardWrapper>
+                  {PrototypesArray.map((prototype, index) => {
+                    return (
+                      <CustomTileWrapper key={index}>
+                        <Tile
+                          style={{
+                            background: `linear-gradient(180deg, rgba(0,0,0,0.5) 0%, rgba(0,0,0,0.8) 80%), url(${prototype.imageUrl}) black 50% / cover no-repeat`,
+                          }}
+                          variant={TileVariant.HOVER}
                         >
-                          More Details
-                        </Button>
-                      </CardBody>
-                    </Card>
-                  );
-                })}
-              </CardWrapper>
+                          <TileFrontGradientFade>
+                            <CustomTileNameWrapper>
+                              <span>{prototype.title}</span>
+                            </CustomTileNameWrapper>
+                            <CustomTileDescriptionWrapper>
+                              <span>{prototype.description}</span>
+                            </CustomTileDescriptionWrapper>
+                          </TileFrontGradientFade>
+                          <TileHover>
+                            <CustomTileNameHoverWrapper>
+                              <span>{prototype.title}</span>
+                            </CustomTileNameHoverWrapper>
+                            <CustomTileDescriptionHoverWrapper>
+                              <span>{prototype.description}</span>
+                            </CustomTileDescriptionHoverWrapper>
+                            <CustomTileActionButtonWrapper>
+                              <Button
+                                size={Size.LARGE}
+                                purpose={Purpose.SECONDARY}
+                                onClick={() => navigate(prototype.redirectionLink)}
+                              >
+                                Open
+                              </Button>
+                            </CustomTileActionButtonWrapper>
+                          </TileHover>
+                        </Tile>
+                      </CustomTileWrapper>
+                    );
+                  })}
+                </CardWrapper>
+              </LandingPageWrapper>
             </main>
           </div>
         </div>
--- src/pages/LandingPage/styles/landing.styles.ts
+++ src/pages/LandingPage/styles/landing.styles.ts
@@ -1,17 +1,129 @@
 import styled from '@emotion/styled';
+import { DARK } from '@spglobal/koi-helpers';
+
+export const LandingPageWrapper = styled.div`
+  width: 50%;
+  margin: auto;
+`;
 
 export const HeaderWrapper = styled.div`
-  margin: 16px 200px;
+  margin: 16px 0px;
 `;
 
 export const CardWrapper = styled.div`
   display: flex;
-  margin: 16px 200px;
+  margin: 16px 0px;
   flex-wrap: wrap;
   column-gap: 10px;
   row-gap: 10px;
 
+  > div > div {
+    width: 310px;
+    height: 310px;
+  }
+`;
+
+export const CustomTileWrapper = styled.div`
   > div {
-    width: 480px;
+    border: 1px solid black;
+
+    &:hover {
+      border: none;
+
+      > div {
+        border-left-color: var(--color-dark-blue-55);
+        border-right-color: var(--color-dark-blue-55);
+        border-bottom-color: var(--color-dark-blue-55);
+      }
+    }
+  }
+
+  bm {
+    color: var(--color-warning);
+  }
+
+  [data-tile-hover] {
+    justify-content: unset;
+  }
+
+  .${DARK} & {
+    > div {
+      border-color: var(--color-base-black);
+      background: var(--color-dark-gray-80);
+    }
+  }
+`;
+
+export const CustomTileNameWrapper = styled.div`
+  color: var(--color-base-white);
+  font-size: var(--size-font-9);
+  font-weight: var(--font-weight-bold);
+  padding: var(--size-space-sm) var(--size-space-0);
+  line-height: 27.6px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+export const CustomTileDescriptionWrapper = styled.div`
+  color: var(--color-base-white);
+  font-size: var(--size-font-5);
+  line-height: 18.9px;
+  margin-bottom: 20px;
+`;
+
+export const CustomTileNameHoverWrapper = styled.div`
+  font-size: var(--size-font-9);
+  font-weight: var(--font-weight-bold);
+  margin-bottom: var(--size-space-lg);
+  line-height: 27.6px;
+
+  .${DARK} & {
+    color: var(--color-base-white);
+  }
+`;
+
+export const CustomTileDescriptionHoverWrapper = styled.div`
+  font-size: var(--size-font-5);
+  line-height: 18.9px;
+  margin-bottom: var(--size-space-lg);
+
+  .${DARK} & {
+    color: var(--color-base-white);
+  }
+`;
+
+export const CustomTileDeleteButtonWrapper = styled.div`
+  margin-bottom: var(--size-space-lg);
+
+  > button {
+    padding: var(--size-space-0);
+    color: var(--color-lib-red-5);
+    font-size: var(--size-font-5);
+
+    &:focus:not([disabled]) {
+      border: none;
+    }
+
+    [data-icon] {
+      color: var(--color-lib-red-5);
+    }
+  }
+`;
+
+export const CustomTileActionButtonWrapper = styled.div`
+  position: absolute;
+  bottom: var(--size-space-0);
+  margin: var(--size-space-lg) var(--size-space-0);
+  width: calc(100% - 32px);
+
+  > button {
+    width: 100%;
+  }
+
+  .${DARK} & {
+    > button {
+      color: var(--color-base-white);
+      background: var(--color-dark-blue-55);
+    }
   }
 `;
